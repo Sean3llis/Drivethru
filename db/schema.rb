ed43f13601f500df5f61f364ecb67c2fa2903a77
SEAN <seane235@gmail.com>
2015-09-24 14:44:16 -0400

    add users w secure pass

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150923160143) do
+ActiveRecord::Schema.define(version: 20150924183603) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -46,11 +46,11 @@ ActiveRecord::Schema.define(version: 20150923160143) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
+    t.string   "name"
     t.string   "email"
-    t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   create_table "vendors", force: :cascade do |t|
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@user = User.new(name: "Bob", email: "bob@bobmail.com")
+	end
+  test "email isn't blank" do
+  	@user.email = "    "
+    assert_not @user.valid?
+  end
 end
