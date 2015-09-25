91e04f3911f6de720e6d2b77bec9c99f7fb1d90a
SEAN <seane235@gmail.com>
2015-09-25 14:42:36 -0400

    basic user model creation form

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   get 'users/new'
 
   resources :vendors
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'vendors#index'
+  root 'users#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
